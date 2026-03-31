f1011e8ad4c48929e4307b57a5d1ede8c5d314b1
polunzh <polunzh@gmail.com>
2026-03-31 20:42:35 +0800

    feat: core domain model

---
+++ internal/model/types.go
@@ -0,0 +1,54 @@
+package model
+
+import "fmt"
+
+func MakeAccountID(provider, email string) string {
+	return fmt.Sprintf("%s:%s", provider, email)
+}
+
+type Account struct {
+	ID          string `json:"id"`
+	Provider    string `json:"provider"`
+	Email       string `json:"email"`
+	DisplayName string `json:"displayName"`
+	CredKey     string `json:"credKey"`
+}
+
+type MessageLocator struct {
+	AccountID string `json:"accountId"`
+	Provider  string `json:"provider"`
+	ID        string `json:"id"`
+}
+
+type Message struct {
+	Locator    MessageLocator `json:"locator"`
+	From       string         `json:"from"`
+	Subject    string         `json:"subject"`
+	ReceivedAt string         `json:"receivedAt"`
+	Unread     bool           `json:"unread"`
+}
+
+type MessageDetail struct {
+	Locator           MessageLocator `json:"locator"`
+	From              string         `json:"from"`
+	To                []string       `json:"to"`
+	CC                []string       `json:"cc"`
+	Subject           string         `json:"subject"`
+	ReceivedAt        string         `json:"receivedAt"`
+	Unread            bool           `json:"unread"`
+	TextBody          string         `json:"textBody"`
+	HTMLBody          string         `json:"-"`
+	HtmlBodyAvailable bool           `json:"htmlBodyAvailable"`
+}
+
+type Draft struct {
+	To        []string
+	Subject   string
+	Body      string
+	InReplyTo *MessageLocator
+}
+
+type ListOptions struct {
+	Limit  int
+	Unread bool
+}
---
+++ internal/model/types_test.go
@@ -0,0 +1,29 @@
+package model_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/zhenqiang/mailbox-cli/internal/model"
+)
+
+func TestAccountID(t *testing.T) {
+	a := model.Account{Provider: "gmail", Email: "you@gmail.com"}
+	a.ID = model.MakeAccountID(a.Provider, a.Email)
+	if a.ID != "gmail:you@gmail.com" {
+		t.Fatalf("got %q, want %q", a.ID, "gmail:you@gmail.com")
+	}
+}
+
+func TestMessageLocatorJSON(t *testing.T) {
+	loc := model.MessageLocator{
+		AccountID: "gmail:you@gmail.com",
+		Provider:  "gmail",
+		ID:        "abc123",
+	}
+	b, _ := json.Marshal(loc)
+	want := `{"accountId":"gmail:you@gmail.com","provider":"gmail","id":"abc123"}`
+	if string(b) != want {
+		t.Fatalf("got %s, want %s", b, want)
+	}
+}
